0c09ff2fbb6c1b1db7fe1c48f2b1b934b03b9418
david <d.kubesa@gmail.com>
2017-08-16 17:56:39 +0200

    Added posibility to save profile from the page as JSON

--- fileToAnalyze.py
+++ fileToAnalyze.py
@@ -5,8 +5,8 @@
 
 #COMPUTERSTOANALYZER = '/media/david/Linux storage/Stratosphere/data/CTU-Normal-3-Public/computers.md'
 #BINETFLOW = '/media/david/Linux storage/Stratosphere/data/CTU-Normal-3-Public/2014-09-12_capture1.binetflow'
-COMPUTERSTOANALYZER = '/media/david/LinuxStorage/Stratosphere/data/for-david-2016-10-05.binetflows/computers.md'
-BINETFLOW = '/media/david/LinuxStorage/Stratosphere/data/for-david-2016-10-05.binetflows/for-david-2016-10-05.binetflows'
+#COMPUTERSTOANALYZER = '/media/david/LinuxStorage/Stratosphere/data/for-david-2016-10-05.binetflows/computers.md'
+#BINETFLOW = '/media/david/LinuxStorage/Stratosphere/data/for-david-2016-10-05.binetflows/for-david-2016-10-05.binetflows'
 #BINETFLOW = '/media/david/LinuxStorage/Stratosphere/data/for-david-2016-10-05.binetflows/prvni-hodina.binetflows'
 #BINETFLOW = '/media/david/LinuxStorage/Stratosphere/data/for-david-2016-10-05.binetflows/prvnihodina147.32.86.78.binetflows'
 
@@ -17,3 +17,7 @@ BINETFLOW = '/media/david/LinuxStorage/Stratosphere/data/for-david-2016-10-05.bi
 
 #COMPUTERSTOANALYZER = '/media/david/Linux storage/Stratosphere/data/CTU-Malware-Capture-Botnet-44/computers.md'
 #BINETFLOW = '/media/david/Linux storage/Stratosphere/data/CTU-Malware-Capture-Botnet-44/capture20110812.binetflow'
+
+
+COMPUTERSTOANALYZER = '/media/david/LinuxStorage/Stratosphere/data/fromfrantisek/computers.md'
+BINETFLOW = '/media/david/LinuxStorage/Stratosphere/data/fromfrantisek/for-david-2016-10-05.binetflow'
