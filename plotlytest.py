bd62d810619e347d6393d70ea80bbc99300d3b35
david <d.kubesa@gmail.com>
2017-09-14 15:18:03 +0200

    Removed experiments from this repository

--- plotlytest.py
+++
@@ -1,9 +0,0 @@
-import plotly.offline as py
-import plotly.graph_objs as go
-
-labels = ['Oxygen','Hydrogen','Carbon_Dioxide','Nitrogen']
-values = [4500,2500,1053,500]
-
-trace = go.Pie(labels=labels, values=values)
-
-py.plot([trace], filename='basic_pie_chart2')
